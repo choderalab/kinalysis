c873a78af246dd2bee35edc80dfaafb3a049f8d2
Sonya Hanson <hansons@gpu-3-8.local>
2016-07-18 17:30:35 -0400

    Added trajectory distribution and total time.

--- kinalysis.py
+++ kinalysis.py
@@ -88,3 +88,88 @@ def shukla_coords(trajectories,KER,Aloop,SRC2):
     flattened_rmsd = np.asarray([val for sublist in rmsd for val in sublist])
 
     return [flattened_rmsd, flattened_difference]
+
+#=============================================================================================
+# Making a PDF Summary of the results
+#=============================================================================================
+
+### Inspired by http://stackoverflow.com/questions/8085520/generating-pdf-latex-with-python-script
+
+def pdf_summary(protein,sim_num,sim_tot,sim_time):
+
+   import argparse
+   import os
+   import subprocess
+
+   content = r'''\documentclass{article}
+   \usepackage[pdftex]{graphicx}
+   \usepackage[margin=0.65in]{geometry}
+   \usepackage{caption}
+   \usepackage{subcaption}
+   \renewcommand{\familydefault}{\sfdefault}
+   \begin{document}
+   \textbf{\huge PDF Summary of %(protein)s kinase \\}
+   \\
+
+   \begin{figure}[!hbp]
+    \begin{subfigure}[b]{0.3\textwidth}
+     \centering
+     \includegraphics[width=\textwidth]{traj_distribution.png}
+     \caption{Trajectory Distribution}
+     \label{fig:traj_dist}
+    \end{subfigure}
+   \hfill
+    \begin{subfigure}[b]{0.3\textwidth}
+     \centering
+     \includegraphics[width=\textwidth]{rmsd.png}
+     \caption{RMSD from initial (longest sim)}
+     \label{fig:rmsd}
+    \end{subfigure}
+   \hfill
+    \begin{subfigure}[b]{0.3\textwidth}
+     \centering
+     \includegraphics[width=\textwidth]{dssp.png}
+     \caption{Secondary Structure (longest sim)}
+     \label{fig:sec_struct}
+    \end{subfigure}
+   \caption{There are %(sim_num)s simulations, totalling %(sim_tot)s us. The longest sim is %(time)s us.}
+   \label{fig:initial_anals}
+   \end{figure}
+
+   \noindent\rule{16cm}{0.4pt}
+   \begin{figure}[h]
+    \centering
+    \includegraphics[width=7cm]{dihedral.png}
+    \caption{DFG dihedral histogram of all sims}
+   \end{figure}
+   \begin{figure}[h]
+    \centering
+    \includegraphics[width=7cm]{shukla.png}
+   \caption{Shukla plot: KDE of all sims}
+   \end{figure}
+   \end{document}
+   '''
+
+   parser = argparse.ArgumentParser()
+   parser.add_argument('-a', '--sim_num', default='%s' %sim_num )
+   parser.add_argument('-b', '--sim_tot', default='%s' %sim_tot )
+   parser.add_argument('-t', '--time', default='%s' % ''.join(map(str, sim_time)) )
+   parser.add_argument('-p', '--protein', default='%s' %protein) 
+   parser.add_argument('-s', '--school', default='My U')
+
+   args = parser.parse_args()
+
+   with open('pdf_summary.tex','w') as f:
+       f.write(content%args.__dict__)
+
+   cmd = ['pdflatex', '-interaction', 'nonstopmode', 'pdf_summary.tex']
+   proc = subprocess.Popen(cmd)
+   proc.communicate()
+
+   retcode = proc.returncode
+   if not retcode == 0:
+       os.unlink('pdf_summary.pdf')
+       raise ValueError('Error {} executing command: {}'.format(retcode, ' '.join(cmd))) 
+
+   os.unlink('pdf_summary.tex')
+   os.unlink('pdf_summary.log')
